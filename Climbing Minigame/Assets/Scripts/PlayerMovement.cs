ddd9175d990a88bf6e99ef320902628e57419994
tyyxu <rhiannonabram@webster.edu>
2025-09-29 20:40:32 -0500

    9/29

--- Climbing Minigame/Assets/Scripts/PlayerMovement.cs
+++ Climbing Minigame/Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 2;
     public float horizontalSpeed = 3;
+    public float rightLimit = 5.5f;
+    public float leftLimit = -5.5f;
 
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            if (this.gameObject.transform.position.x < leftLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            }
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+            if (this.gameObject.transform.position.x < rightLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+            }
         }
     }
 }
